414597da5d14fd3fc5620a725f77b131e2b08bb6
rainbowsnail <763118287@qq.com>
2016-11-05 21:17:46 +0800

    Update multihoming_server.c

--- applications/example/multihoming_server.c
+++ applications/example/multihoming_server.c
@@ -11,8 +11,8 @@
 #include "dagaddr.hpp"
 
 #define MAX_XID_SIZE 100
-#define MUL_SERVER1 "www_s.multihong_server1.aaa.xia"
-#define MUL_SERVER2 "www_s.multihong_server2.aaa.xia"
+#define NAME "www_s.multihong_server.aaa.xia"
+
 
 /*
 char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
@@ -63,47 +63,15 @@ void die(int ecode, const char *fmt, ...)
 	exit(ecode);
 }
 
-void *server(void * id){
+void *server(void *socketid){
 	int sock;
 	char buf[XIA_MAXBUF];
 	sockaddr_x cdag;
 	socklen_t dlen;
 	int n;
-	int ser_id = *((int*)id);
-	char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
-	// Generate an SID to use
-	if (XmakeNewSID(sid_string, sizeof(sid_string))) {
-		die(-1, "Unable to create a temporary SID");
-	}
 	
-	say("%d Datagram service started\n", ser_id);
+	say("Datagram service started\n");
 
-	if ((sock = Xsocket(AF_XIA, SOCK_DGRAM, 0)) < 0)
-		die(-2, "unable to create the datagram socket\n");
-
-	struct addrinfo *ai;
-	if (Xgetaddrinfo(NULL, sid_string, NULL, &ai) != 0)
-		die(-1, "getaddrinfo failure!\n");
-
-	sockaddr_x *sa = (sockaddr_x*)ai->ai_addr;
-
-	Graph g((sockaddr_x*)ai->ai_addr);
-	printf("\n%d Datagram DAG\n%s\n", ser_id, g.dag_string().c_str());
-	
-    
-	if(ser_id == 1){
-		if (XregisterName(MUL_SERVER1, sa) < 0 )
-			die(-1, "error registering name: %s\n", MUL_SERVER1);
-		say("registered name: \n%s\n", MUL_SERVER1);
-	}
-	else{
-		if (XregisterName(MUL_SERVER2, sa) < 0 )
-			die(-1, "error registering name: %s\n", MUL_SERVER2);
-		say("registered name: \n%s\n", MUL_SERVER2);
-	}
-	if (Xbind(sock, (sockaddr *)sa, sizeof(sa)) < 0) {
-		die(-3, "unable to bind to the dag\n");
-	}
 
 	pid_t pid = 0;
 
@@ -129,24 +97,72 @@ void *server(void * id){
 
 	Xclose(sock);
 }
-int main()
-{
-	pthread_t *clients = (pthread_t*)malloc(2 * sizeof(pthread_t));
 
-	if (!clients)
-		die(-5, "Unable to allocate threads\n");
-	int id1 = 1;//(int*)malloc(sizeof(int));
-	int id2 = 2;//(int*)malloc(sizeof(int));
-	//*id1 = 1;
-	//*id2 = 2;
-	pthread_create(&clients[0], NULL, server, (void *)&id1);
-	pthread_create(&clients[1], NULL, server, (void *)&id2);
-	
-	for (int i = 0; i < 2; i++) {
-		pthread_join(clients[i], NULL);
+int registerReceiver()
+{
+	int sock;
+	char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
+
+	//say ("\n%s (%s)\n", TITLE, VERSION);
+	//say("Service Name: %s\n", name);
+	//say("Root Directory: %s\n", rootdir);
+	//say("Max Chunk Size: %u\n\n", chunksize);
+
+	// create a socket, and listen for incoming connections
+	if ((sock = Xsocket(AF_XIA, SOCK_STREAM, 0)) < 0)
+		die(-1, "Unable to create the listening socket\n");
+
+	struct addrinfo *ai;
+
+	if(XmakeNewSID(sid_string, sizeof(sid_string))) {
+		die(-1, "Unable to create a temporary SID");
 	}
 
-	free(clients);
+	if (Xgetaddrinfo(NULL, sid_string, NULL, &ai) != 0)
+		die(-1, "getaddrinfo failure!\n");
+
+	sockaddr_x *dag = (sockaddr_x*)ai->ai_addr;
+	if (XregisterName(NAME, dag) < 0 )
+		die(-1, "error registering name: %s\n", NAME);
+
+	if (Xbind(sock, (struct sockaddr*)dag, sizeof(dag)) < 0) {
+		Xclose(sock);
+		die(-1, "Unable to bind to the dag: %s\n", dag);
+	}
+
+	Xlisten(sock, 5);
+
+	Graph g(dag);
+	say("listening on dag: %s\n", g.dag_string().c_str());
+
+	return sock;
+}
+
+void *blockingListener(void *socketid)
+{
+	int sock = *((int*)socketid);
+	int acceptSock;
+	while (1) {
+		say("Waiting for a client connection\n");
+
+		if ((acceptSock = Xaccept(sock, NULL, NULL)) < 0)
+			die(-1, "accept failed\n");
+
+		say("connected\n");
+
+		// handle the connection in a new thread
+		pthread_t client;
+		pthread_create(&client, NULL, server, (void *)&acceptSock);
+	}
+
+	Xclose(sock); // we should never reach here!
+	return NULL;
+}
+
+int main()
+{
+	int sock = registerReceiver();
+	blockingListener((void *)&sock);
 	
 	return 0;
 }
