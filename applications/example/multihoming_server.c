25c8954bc25825cea19feb0a035ea196b1483565
rainbowsnail <763118287@qq.com>
2016-11-04 22:52:10 -0400

    Add files via upload
    add multihoming test files

---
+++ applications/example/multihoming_server.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/wait.h>
+#ifdef __APPLE__
+#include <libgen.h>
+#endif
+#include "Xsocket.h"
+#include "Xkeys.h"
+#include "dagaddr.hpp"
+
+#define MAX_XID_SIZE 100
+#define NAME "www_s.multihong_server.aaa.xia"
+#define SID_SERVER   "SID:0f00000000000000000000000000000000008888"
+/*
+char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
+	// Generate an SID to use
+	if (XmakeNewSID(sid_string, sizeof(sid_string))) {
+		die(-1, "Unable to create a temporary SID");
+	}
+*/
+/*
+** write the message to stdout unless in quiet mode
+*/
+void say(const char *fmt, ...)
+{
+	if (verbose) {
+		va_list args;
+
+		va_start(args, fmt);
+		vprintf(fmt, args);
+		va_end(args);
+	}
+}
+
+/*
+** always write the message to stdout
+*/
+void warn(const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	vfprintf(stdout, fmt, args);
+	va_end(args);
+
+}
+
+/*
+** write the message to stdout, and exit the app
+*/
+void die(int ecode, const char *fmt, ...)
+{
+	va_list args;
+
+	va_start(args, fmt);
+	vfprintf(stdout, fmt, args);
+	va_end(args);
+	fprintf(stdout, "%s: exiting\n", TITLE);
+	exit(ecode);
+}
+
+int main()
+{
+	int sock;
+	char buf[XIA_MAXBUF];
+	sockaddr_x cdag;
+	socklen_t dlen;
+	int n;
+	char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
+	// Generate an SID to use
+	if (XmakeNewSID(sid_string, sizeof(sid_string))) {
+		die(-1, "Unable to create a temporary SID");
+	}
+	
+	say("Datagram service started\n");
+
+	if ((sock = Xsocket(AF_XIA, SOCK_DGRAM, 0)) < 0)
+		die(-2, "unable to create the datagram socket\n");
+
+	struct addrinfo *ai;
+	if (Xgetaddrinfo(NULL, SID_SERVER, NULL, &ai) != 0)
+		die(-1, "getaddrinfo failure!\n");
+
+	sockaddr_x *sa = (sockaddr_x*)ai->ai_addr;
+
+	Graph g((sockaddr_x*)ai->ai_addr);
+	printf("\nDatagram DAG\n%s\n", g.dag_string().c_str());
+
+    if (XregisterName(DGRAM_NAME, sa) < 0 )
+    	die(-1, "error registering name: %s\n", DGRAM_NAME);
+	say("registered name: \n%s\n", DGRAM_NAME);
+
+	if (Xbind(sock, (sockaddr *)sa, sizeof(sa)) < 0) {
+		die(-3, "unable to bind to the dag\n");
+	}
+
+	pid_t pid = 0;
+
+	while (1) {
+		say("Dgram Server waiting\n");
+
+		dlen = sizeof(cdag);
+		memset(buf, 0, sizeof(buf));
+		if ((n = Xrecvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr *)&cdag, &dlen)) < 0) {
+			warn("Recv error on socket %d, closing connection\n", pid);
+			break;
+		}
+
+		say("server received %d bytes\n", n);
+		
+		if ((n = Xsendto(sock, buf, n, 0, (struct sockaddr *)&cdag, dlen)) < 0) {
+			warn("%5d send error\n", pid);
+			break;
+		}
+
+		say("server sent %d bytes\n", n);
+	}
+
+	Xclose(sock);
+	
+	return 0;
+}
