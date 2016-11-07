75e611529597c9c2dfc92ba9b187d52fc9cdc4ba
rainbowsnail <763118287@qq.com>
2016-11-07 09:45:03 +0800

    Update xftp_adv_client.c

--- applications/xftp/stage/xftp_adv_client.c
+++ applications/xftp/stage/xftp_adv_client.c
@@ -1,295 +1,183 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "stage_utils.h"
 #include <time.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <signal.h>
-#include <poll.h>
-#ifdef __APPLE__
-#include <libgen.h>
-#endif
-#include <unistd.h>
-#include "Xsocket.h"
-#include "dagaddr.hpp"
-#include "dagaddr.h"
-#include "Xkeys.h"
-#include "Xsocket.h"
-
-#define NAME "www_s.multihong_server1.aaa.xia"
-#define MAX_BUF_SIZE 62000
 
 
-int verbose = 1;	// display all messages
-int delay = 100000;		// don't delay between loops
-int loops = 100;		// only do 1 pass
-int pktSize = 512;	// default pkt size
-int reconnect = 0;	// don't reconnect between loops
-int threads = 1;	// just a single thread
-int terminate = 0;  // sighandler sets to 1 if we should quit
+#define VERSION "v1.0"
+#define TITLE "XIA Advanced FTP client"
 
-struct addrinfo *ai;
+using namespace std;
 
-/*
-** write the message to stdout unless in quiet mode
-*/
-void say(const char *fmt, ...)
+bool stage = true;
+
+char fin[256], fout[256];
+
+int ftpSock, stageManagerSock;
+
+XcacheHandle h;
+int getFile(int sock)
 {
-	if (verbose) {
-		va_list args;
+    FILE *fd = fopen(fout, "w");
+    ofstream logFile("clientTime.log");
+    int n;
+    int status = -1;
+    char send_to_server_[XIA_MAX_BUF];
+    memset(send_to_server_, '\0', strlen(send_to_server_));
+    sprintf(send_to_server_, "finish receiveing the current CIDs.");
 
-		va_start(args, fmt);
-		vprintf(fmt, args);
-		va_end(args);
-	}
+    int chunkSock;
+    if ((chunkSock = Xsocket(AF_XIA, SOCK_STREAM, 0)) < 0) {
+        die(-1, "unable to create chunk socket\n");
+    }
+
+    char cmd[XIA_MAX_BUF];
+    char reply[XIA_MAX_BUF];
+
+    vector<string> CIDs;
+
+    // send the file request to the xftp server
+    sprintf(cmd, "get %s", fin);
+    sendStreamCmd(sock, cmd);
+    int fetchTime = 0;
+
+    // receive the CID list from xftp server
+    while (1) {
+        say("In getFile, receive CID list from xftp server.\n");
+        memset(reply, '\0', XIA_MAX_BUF);
+        if ((n = Xrecv(sock, reply, sizeof(reply), 0)) < 0) {
+            Xclose(sock);
+            die(-1, "Unable to communicate with the server\n");
+        }
+        if (strncmp(reply, "cont", 4) == 0) {
+            say("Receiving partial chunk list\n");
+            char reply_arr[strlen(reply)];
+            //char reply_arr[strlen(reply+5)];
+            strcpy(reply_arr, reply + 5);
+            char *cid;
+            cid = strtok(reply_arr, " ");
+            CIDs.push_back(cid);
+say("---------CID:%s\n",cid);
+            // register CID
+            while ((cid = strtok(NULL, " ")) != NULL) {
+                CIDs.push_back(cid);
+say("---------CID:%s\n",cid);
+            }
+        }
+
+        else if (strncmp(reply, "done", 4) == 0) {
+            say("Finish receiving all the CIDs\n");
+            break;
+        }
+        sayHello(sock, send_to_server_);
+    }
+
+    // update CID list to the local staging service if exists.
+    // Send CID list to stage manager
+    if (stage) {
+        if ((n = updateManifest(stageManagerSock, CIDs) < 0)) {
+            close(stageManagerSock);
+            die(-1, "Unable to communicate with the local prefetching service\n");
+        }
+        say("After updateManifest\n");
+    }
+    XcacheHandleInit(&h);
+
+    vector<unsigned int> chunkSize; // in bytes
+    vector<long> latency;
+    vector<long> chunkStartTime;
+    vector<long> chunkFinishTime;
+
+    long startTime = now_msec();
+    unsigned int bytes = 0;
+
+    // chunk fetching begins
+    char data[CHUNKSIZE];
+    for (unsigned int i = 0; i < CIDs.size(); i++) {
+
+        say("The number of chunks is %d.\n", CIDs.size());
+        say("Fetching chunk %u / %lu\n", i, CIDs.size() - 1);
+        int len;
+        sockaddr_x addr;
+        if (stage) {
+            memset(cmd, 0, sizeof(cmd));
+            sprintf(cmd, "fetch");
+            sprintf(cmd, "%s %s Time: %d", cmd, CIDs[i].c_str(), fetchTime);
+            say("CMD is :%s\n", cmd);
+            if (send(stageManagerSock, cmd, strlen(cmd), 0) < 0) {
+                die(-1, "send cmd fail! cmd is %s", cmd);
+            }
+            say("After send Fetch!\n");
+            memset(cmd, 0, sizeof(cmd));
+            if ((len = recv(stageManagerSock, cmd, XIA_MAX_BUF, 0)) < 0) {
+                die(-1, "fail to recv from stageManager!");
+            }
+            say("Get NewDag: %s\n", cmd);
+            url_to_dag(&addr, cmd, len);
+        }
+        else {
+            url_to_dag(&addr, (char*)CIDs[i].c_str(), CIDs[i].size());
+say("CID=%s %d\n",(char*)CIDs[i].c_str(), CIDs[i].size());
+        }
+        long start_time = now_msec();
+        if ((len = XfetchChunk(&h, data, CHUNKSIZE, XCF_BLOCK | XCF_SKIPCACHE, &addr, sizeof(addr))) < 0) {
+            die(-1, "XcacheGetChunk Failed\n");
+        }
+        long end_time = now_msec();
+        char req[256];
+        dag_to_url(req,256,&addr);
+        fetchTime = end_time - start_time;
+        logFile << i <<" Chunk. Running time is: " << end_time - start_time << " ms. req: " << req << endl;
+        say("writing %d bytes of chunk %s to disk\n", len, string2char(CIDs[i]));
+        fwrite(data, 1, len, fd);
+        bytes += len;
+        chunkSize.push_back(len);
+        latency.push_back(end_time - start_time);
+        chunkStartTime.push_back(start_time);
+        chunkFinishTime.push_back(now_msec());
+    }
+    fclose(fd);
+    long finishTime = now_msec();
+    logFile << "Received file " << fout << " at "<< (1000 * (float)bytes / 1024) / (float)(finishTime - startTime) << " KB/s (Time: " << finishTime - startTime << " msec, Size: " << bytes << "bytes)\n";
+    sendStreamCmd(sock, "done");    // chunk fetching ends, send command to server
+    for (unsigned int i = 0; i < CIDs.size(); i++) {
+        logFile << fout << "\t" << CIDs[i] << "\t" << chunkSize[i] << " B\t" << latency[i] << "\t" << chunkStartTime[i] << "\t" << chunkFinishTime[i] << endl;
+    }
+    Xclose(chunkSock);
+    Xclose(sock);
+    return status;
 }
 
-/*
-** always write the message to stdout
-*/
-void warn(const char *fmt, ...)
-{
-	va_list args;
-
-	va_start(args, fmt);
-	vfprintf(stdout, fmt, args);
-	va_end(args);
-
-}
-
-/*
-** write the message to stdout, and exit the app
-*/
-void die(int ecode, const char *fmt, ...)
-{
-	va_list args;
-
-	va_start(args, fmt);
-	vfprintf(stdout, fmt, args);
-	va_end(args);
-	fprintf(stdout, "exiting\n");
-	exit(ecode);
-}
-
-/*
-** create a semi-random alphanumeric string of the specified size
-*/
-char *randomString(char *buf, int size)
-{
-	int i;
-	static const char *filler = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	static int refresh = 1;
-	int samples = strlen(filler);
-
-	if (!(--refresh)) {
-		// refresh rand every now and then so it doesn't degenerate too much
-		// use a prime number to keep it interesting
-		srand(time(NULL));
-		refresh = 997;
-	}
-	for (i = 0; i < size - 1; i ++) {
-		buf[i] = filler[rand() % samples];
-	}
-	buf[size - 1] = 0;
-
-	return buf;
-}
-
-/*
-** do one send/receive operation on the specified socket
-*/
-int process(int sock)
-{
-	int size;
-	int sent = 0;
-	int received = 0;
-	char buf1[MAX_BUF_SIZE + 1], buf2[MAX_BUF_SIZE + 1];
-
-	if (pktSize == 0)
-		size = (rand() % MAX_BUF_SIZE) + 1;
-	else
-		size = pktSize;
-	randomString(buf1, size);
-	printf("%d\n", (int)size);
-	int count = size;
-
-	sent = Xsend(sock, buf1, count, 0);
-	if (sent < 0)
-	{
-		die(-4, "Send error %d on socket %d\n", errno, sock);
-	}
-
-	say("Xsock %4d sent %d of %d bytes\n", sock, sent, size);
-
-	int rc;
-	struct pollfd pfds[2];
-	pfds[0].fd = sock;
-	pfds[0].events = POLLIN;
-	if ((rc = Xpoll(pfds, 1, 50000)) <= 0) {
-		die(-5, "Poll returned %d\n", rc);
-	}
-
-	memset(buf2, 0, sizeof(buf2));
-	count = 0;
-	while (sent != received && (count = Xrecv(sock, &buf2[received], sizeof(buf2) - received, 0)) > 0) {
-		say("%5d received %d bytes\n", sock, count);
-		received += count;
-		buf2[received] = 0;
-	}
-
-	say("Xsock %4d received %d bytes in total\n", sock, received);
-
-	if (sent != received || strcmp(buf1, buf2) != 0)
-		warn("Xsock %4d received data different from sent data! (bytes sent/recv'd: %d/%d)\n",
-				sock, sent, received);
-
-	return 0;
-}
-
-/*
-** do a short pause between operations
-*/
-void pausex()
-{
-	int t;
-	if (delay == -1)
-		// default - don't pause at all
-		return;
-	else if (delay == 0)
-		// pause for some random period less than a second
-		t = rand() % 1000000;
-	else
-		// pause for the specfied number of hundredths of a second
-		t = delay;
-	usleep(t);
-}
-
-/*
-** create a socket and connect to the remote server
-*/
-int flag = 0;
-int connectToServer(struct ifaddrs *ifa)
-{
-	int ssock;
-	if ((ssock = Xsocket(AF_XIA, XSOCK_STREAM, 0)) < 0) {
-		die(-2, "unable to create the server socket\n");
-	}
-	say("Xsock %4d created\n", ssock);
-	
-	char sid_string[strlen("SID:") + XIA_SHA_DIGEST_STR_LEN];
-	// Generate an SID to use
-	if (XmakeNewSID(sid_string, sizeof(sid_string))) {
-		die(-1, "Unable to create a temporary SID");
-	}
-	
-	struct addrinfo hints, *ai, *sai;
-	sockaddr_x *sa;
-	bzero(&hints, sizeof(hints));
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_family = AF_XIA;
-	hints.ai_flags |= XAI_DAGHOST;
-	
-	Graph g((sockaddr_x *) ifa->ifa_addr);
-	Xgetaddrinfo(g.dag_string().c_str(), sid_string, &hints, &ai);  // hints should have the XAI_DAGHOST flag set
-	//Use the sockaddr_x returned by Xgetaddrinfo in your next call
-	
-	sa = (sockaddr_x*)ai->ai_addr;
-	Xbind(ssock, (struct sockaddr*)sa, sizeof(sa));
-	Graph gg(sa);
-	printf("\n%s\n", gg.dag_string().c_str());
-	
-	sockaddr_x dag;
-	socklen_t daglen = sizeof(dag);
-	
-	//if (Xgetaddrinfo(MUL_SERVER1, NULL, NULL, &sai) != 0)
-	//	die(-1, "unable to lookup name %s\n", MUL_SERVER1);
-	if (XgetDAGbyName(NAME, &dag, &daglen) < 0)
-		die(-1, "unable to locate: %s\n", NAME);
-	
-	if (Xconnect(ssock, (struct sockaddr *)&dag, sizeof(sockaddr_x)) < 0)
-		die(-3, "unable to connect to the destination dag\n");
-
-	say("Xsock %4d connected\n", ssock);
-
-	return ssock;
-}
-
-/*
-** the main loop for talking to the remote server
-** when threading is enabled, one of these will run in each thread
-**
-** The parameter and return code are there to satisify the thread library
-*/
-void *mainLoop(void * ifaddr)
-{
-	struct ifaddrs *ifa = (struct ifaddrs *)ifaddr;
-	int ssock;
-	int count = 0;
-	int printcount = 1;
-	
-	if (loops == 1)
-		printcount = 0;
-
-	//struct sockaddr * sa = ifa->ifa_addr;
-	ssock = connectToServer(ifa);
-	
-	while (true) {		
-		say("Xsock %4d loop #%d\n", ssock, count);
-		if (process(ssock) != 0)
-			break;
-		pausex();
-		count++;
-		if (loops > 0 && count == loops)
-				break;
-	}
-	Xclose(ssock);
-	say("Xsock %4d closed\n", ssock);
-	return NULL;
-}
-
-/*
-** where it all happens
-*/
 int main(int argc, char **argv)
 {
-	srand(time(NULL));
-	//getConfig(argc, argv);
+    while (1) {
+        char myAD[MAX_XID_SIZE];
+        char myHID[MAX_XID_SIZE];
 
-	int family, s;  
-    char host[NI_MAXHOST];
-	struct ifaddrs *ifa=NULL, *if1=NULL, *if2=NULL;
-	
-	struct ifaddrs *ifaddr = NULL;
-	if( Xgetifaddrs(&ifaddr) < 0){
-		die(-1, "Xgetifaddrs failed");
-	}
-	
-	for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {  
-        if (ifa->ifa_addr == NULL)  
-            continue;  
-    
-        printf("interface: %s \n", ifa->ifa_name);  
-		for(int i=0;i<20;++i){
-		printf("%c",(ifa->ifa_name)[i]);
-		}		
-		printf("\n");
-		if(strcmp(ifa->ifa_name, "iface2")==0) if1 = ifa;//"wlp6s0"
-		if(strcmp(ifa->ifa_name, "iface3")==0 ) if2 = ifa;//"wlx60a44ceca928"
-    } 
-	
-	pthread_t *clients = (pthread_t*)malloc(2 * sizeof(pthread_t));
+        char ftpServAD[MAX_XID_SIZE];
+        char ftpServHID[MAX_XID_SIZE];
+        say("In main function of xftp_adv_client.\n");
+        if (argc == 2) {
+            say("\n%s (%s): started\n", TITLE, VERSION);
+            strcpy(fin, argv[1]);
+            sprintf(fout, "my%s", fin);
 
-	if (!clients)
-		die(-5, "Unable to allocate threads\n");
+            ftpSock = initStreamClient(getXftpName(), myAD, myHID, ftpServAD, ftpServHID);
+            //stageManagerSock is used to communicate with stage manager.
+            //change it into Unix domain socket
+            stageManagerSock = registerUnixStageManager(UNIXMANAGERSOCK);
+            //stageManagerSock = registerStageManager(getStageManagerName());
+            say("The current stageManagerSock is %d\n", stageManagerSock);
+            if (stageManagerSock < 0) {
+                say("No local staging service running\n");
+                stage = false;
+            }
+            getFile(ftpSock);
+            close(stageManagerSock);
+            return 0;
+        }
+        else {
+            die(-1, "Please input the filename as the second argument\n");
+        }
+    }
 
-	pthread_create(&clients[0], NULL, mainLoop, (void *)if1);
-	pthread_create(&clients[1], NULL, mainLoop, (void *)if2);
-	
-	for (int i = 0; i < 2; i++) {
-		pthread_join(clients[i], NULL);
-	}
-
-	free(clients);
-	
-	return 0;
+    return 0;
 }
