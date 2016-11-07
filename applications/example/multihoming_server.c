7a7027e68948c3b4704525d99f3029383ab42003
rainbowsnail <763118287@qq.com>
2016-11-07 10:39:18 +0800

    Update multihoming_server.c

--- applications/example/multihoming_server.c
+++ applications/example/multihoming_server.c
@@ -80,14 +80,14 @@ void *server(void *socketid){
 
 		dlen = sizeof(cdag);
 		memset(buf, 0, sizeof(buf));
-		if ((n = Xrecv(sock, buf, sizeof(buf)) < 0) {
+		if ((n = Xrecv(sock, buf, sizeof(buf), 0) < 0) {
 			warn("Recv error on socket %d, closing connection\n", pid);
 			break;
 		}
 
 		say("server received %d bytes\n", n);
 		
-		if ((n = Xsend(sock, buf, n) < 0) {
+		if ((n = Xsend(sock, buf, n, 0)) < 0) {
 			warn("%5d send error\n", pid);
 			break;
 		}
