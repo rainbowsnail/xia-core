0fef168a35528a6f4c6c284261fe9f1e6e40c773
rainbowsnail <763118287@qq.com>
2016-11-09 06:41:36 -0500

    Add files via upload
    Test iwlist

---
+++ applications/xftp/stage/switchAP.c
@@ -0,0 +1,24 @@
+#include "stage_utils.h"
+
+//
+
+#define SCAN_INTERVAL 10
+#define GET_SSID_LIST "iwlist wlx60a44ceca928 scanning"//| grep '\\\"[a-zA-Z _0-9.]*\\\"' -o"
+//#define 
+
+int main(){
+	string ssid_list;// = execSystem(GET_SSID_LIST);
+	if (ssid.empty()) {
+// cerr<<"No network\n";
+		while (1) {			
+			ssid_list = execSystem(GETSSID_CMD);
+			printf("ssid list:\n %s ssid_list", ssid_list);
+			if (!ssid.empty()) {
+// cerr<<"Network back\n";
+				break;
+			}
+			usleep(SCAN_INTERVAL * 1000);
+		}
+	}
+	return 0;
+}
