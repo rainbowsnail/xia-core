2db006fdf5782fdcb30dad6f93c0f145ea43ef16
Jing <763118287@qq.com>
2016-12-06 20:11:12 +0800

    Update policy_manager.cc

--- daemons/xcache/policy_manager.cc
+++ daemons/xcache/policy_manager.cc
@@ -5,7 +5,7 @@ bool xcache_policy_manager::cacheable(xcache_meta *meta)
 {
 	int ret = -1;
 
-	std::cout << "PolicyManager: Store\n";
+	//std::cout << "PolicyManager: Store\n";
 	for(std::vector<xcache_eviction_policy *>::iterator i = policy_vector.begin(); i != policy_vector.end(); ++i) {
 		ret = (*i)->store(meta);
 	}
